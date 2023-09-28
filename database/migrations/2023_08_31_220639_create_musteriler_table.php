2cf75fe28ce038fb6037429cc002f27f3ef8c0ec
iskender <iskender-1301@hotmail.com>
2023-09-28 14:58:10 +0300

    ++

--- database/migrations/2023_08_31_220639_create_musteriler_table.php
+++ database/migrations/2023_08_31_220639_create_musteriler_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('ad');
             $table->string('soyad');
             $table->string('email')->unique();
-            $table->string('sifre');
         });
     }
 
